1cdd56198d9227de6ff680e6e8f148145b857102
himadri2110 <18it.himadri.shah@gmail.com>
2022-01-05 23:03:29 +0530

    feat: footer

--- src/App.js
+++ src/App.js
@@ -12,44 +12,48 @@ import About from "./pages/About";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <header>
-          <NavLink
-            className="nav-link"
-            to="/"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "bold" : "normal",
-            })}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            className="nav-link"
-            to="/about"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "bold" : "normal",
-            })}
-          >
-            About
-          </NavLink>
-          <NavLink
-            className="nav-link"
-            to="/product"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "bold" : "normal",
-            })}
-          >
-            Product
-          </NavLink>
-        </header>
+      <div className="router">
+        <Router>
+          <header>
+            <NavLink
+              className="nav-link"
+              to="/"
+              style={({ isActive }) => ({
+                fontWeight: isActive ? "bold" : "normal",
+              })}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className="nav-link"
+              to="/about"
+              style={({ isActive }) => ({
+                fontWeight: isActive ? "bold" : "normal",
+              })}
+            >
+              About
+            </NavLink>
+            <NavLink
+              className="nav-link"
+              to="/product"
+              style={({ isActive }) => ({
+                fontWeight: isActive ? "bold" : "normal",
+              })}
+            >
+              Product
+            </NavLink>
+          </header>
 
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/product" element={<Product />}></Route>
-          <Route path="/product/:id" element={<Product />}></Route>
-        </Routes>
-      </Router>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/product" element={<Product />}></Route>
+            <Route path="/product/:id" element={<Product />}></Route>
+          </Routes>
+        </Router>
+      </div>
+
+      <footer>Himadri Shah</footer>
     </div>
   );
 }
